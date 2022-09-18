44ce84fb1677d30ced7c0a90fe3c5042177b71e2
Anthony Quisenberry <aquisenberry@Samanthas-MacBook-Pro.local>
2022-09-17 22:36:14 -0400

    add logo to header

--- src/components/Navigation/Header/index.jsx
+++ src/components/Navigation/Header/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Logo from "../../../assets/images/media-lists-logo.png"
 import './header.css'
 const Header = () => {
     return (
+      <>
+      <div className="logo-container">
+        <img className="logo" src={Logo} alt="Logo"/>
+      </div>
       <nav className="navigation">
           <Link className="navigation__link" to="/"> Home</Link>
           <Link className="navigation__link" to="/lists"> My Lists</Link>
           <Link className="navigation__link" to="/search"> Search Media</Link>
       </nav>
+      </>
     );
   };
   
